8c941157d748c2939d93c4cd626fff41fb4d497c
PhamHieu <hieu3112005@gmail.com>
2026-03-28 18:37:15 +0700

    Khởi tạo Base Project, cấu hình CSDL MySQL

---
+++ src/main/java/com/nhom586/ktxmanagement/KtxManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nhom586.ktxmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtxManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtxManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhom586/ktxmanagement/KtxManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom586.ktxmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtxManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
